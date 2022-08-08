78cefeeceb6d18b64d0f10bad815c39b87c55925
Balaji Yaswanth Vankala <balajiyaswanth.v@gmail.com>
2022-08-09 03:27:52 +0530

    installed theme-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+// import '@hackclub/theme/fonts/reg-bold.css'
+import { ThemeProvider } from 'theme-ui'
+import theme from '@hackclub/theme'
 
 function App() {
   return (
@@ -7,6 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <ThemeProvider theme={theme}>Hello</ThemeProvider>
+      {/* <h3>Hello</h3> */}
+      {/* <button className='outlineLg'>Click me</button> */}
     </div>
   );
 }
